a4f1f28d131d4db4abbc8f566685710ddafde791
Bijaya Manandhar <bijayamanandhar@Bijayas-MacBook-Pro.local>
2018-02-10 08:54:07 -0800

    trying to replace data by metric units available on the api url

--- script.js
+++ script.js
@@ -1,26 +1,27 @@
 $(document).ready(function(){
     var api_key = "8f236e524e673e139b0f6739b81a6eb4";
     var celsius = false;
+//    var openWeatherMap; Trying to replace data by metric units available on the api url, 
     var ipInfo = "http://ipinfo.io";
     var imperial = "http://api.openweathermap.org/data/2.5/weather?units=imperial&lat=";
-    var metric = "http://api.openweathermap.org/data/2.5/weather?units=metric&lat=";
+//    var metric = "http://api.openweathermap.org/data/2.5/weather?units=metric&lat=";
 
     function result(fahrenheit, c){
-        if(c) return Math.round(((fahrenheit - 32) * (5/9))*10)/10 + "&deg" + "C";
-        return Math.round(fahrenheit*10)/10 + "&deg" + "F";
+        if(c) return Math.round(((fahrenheit - 32) * (5/9)) * 10) / 10 + "&deg" + "C";
+        return Math.round(fahrenheit * 10) / 10 + "&deg" + "F";
     }
 
     function render(data, celsius){
         console.log(data);
         var temp = result(data.main.temp, celsius);
-        var high = result(data.main.temp_max, celsius);
-        var low = result(data.main.temp_min, celsius);
+        var max = result(data.main.temp_max, celsius);
+        var min = result(data.main.temp_min, celsius);
 
         $(".city").html("IP address: " + data.name + ", " + data.sys.country);
         $(".temp").html(temp);
         $(".weather").html(data.weather[0].description);
-        $(".max").html("Max temp: " + high);
-        $(".min").html("Min temp: " + low);
+        $(".max").html("Max temp: " + max);
+        $(".min").html("Min temp: " + min);
         $(".humidity").html("Humidity: " + data.main.humidity + "%");
         $(".pressure").html("Pressure: " + data.main.pressure + "hPa");
         $(".wind-speed").html("Wind speed: " + data.wind.speed + "mile/hr");
