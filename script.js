2f2db0ae616c00c6f64ffbd1b7b1dfc63ddfd92f
Bijaya Manandhar <bijayamanandhar@Bijayas-MacBook-Pro.local>
2018-02-09 22:00:16 -0800

    many changes

--- script.js
+++ script.js
@@ -1,50 +1,50 @@
+$(document).ready(function(){
 var api_key = "8f236e524e673e139b0f6739b81a6eb4";
-var cel = false;
-var wd;
+var celsius = false;
 
 function displayTemp(fTemp, c){
-  if(c) return Math.round((fTemp - 32) * (5/9)) + "&deg" + "C";
-  return Math.round(fTemp) + "&deg" + "F";
+    if(c) return Math.round((fTemp - 32) * (5/9)) + "&deg" + "C";
+    return Math.round(fTemp) + "&deg" + "F";
 }
 
-function render(wd, cel){
-      var city = "You are in " + wd.name;
-      var icon = wd.weather[0].icon;
-      var weather = wd.weather[0].description;
-      var temp = displayTemp(wd.main.temp, cel);
-      var high = displayTemp(wd.main.temp_max, cel);
-      var low = displayTemp(wd.main.temp_min, cel);
-      var pressure = wd.main.pressure;
-      var windDir = wd.wind.deg;
+function render(data, celsius){
+    console.log(data);
+    var country = data.sys.country;
+    var icon = data.weather[0].icon;
+    var iconSrc = "http://openweathermap.org/img/w/" + icon + ".png";
+    var temp = displayTemp(data.main.temp, celsius);
+    var high = displayTemp(data.main.temp_max, celsius);
+    var low = displayTemp(data.main.temp_min, celsius);
 
-      $('.city').html(city + ", " + wd.sys.country);
-      var icon = "http://openweathermap.org/img/w/" + icon + ".png";
-      $('.temp').prepend('<img src="' + icon + '">');
-      $('.temp').html(temp);
-      $('.weather').html(weather);
-      $('.max').html("Max temp: "+high);
-      $(".min").html("Min temp: " + low);
-      $(".pressure").html("Pressure: " + pressure +"hPa");
-      $(".wind-dir").html("Wind direction: " + windDir + "&#176");
+    $('.city').html("IP address: " + data.name + ", " + country);
+    $('.temp').prepend('<img src="' + icon + '">');
+    $('.temp').html(temp);
+    $('.weather').html(data.weather[0].description);
+    $('.max').html("Max temp: "+high);
+    $(".min").html("Min temp: " + low);
+    $(".pressure").html("Pressure: " + data.main.pressure +"hPa");
+    $(".wind-speed").html("Wind speed: " + data.wind.speed + "m/s");
+    $(".wind-dir").html("Wind direction: " + data.wind.deg + "&#176");
+    $('.temp').prepend('<img src="' + iconSrc + '">');
 }
 
 $(function(){
-
-  var loc;
-  $.getJSON('http://ipinfo.io', function(d){
-    console.log("assigning the data...")
+    var location;
+    var ipinfo = 'http://ipinfo.io';
+    var openAPI = 'http://api.openweathermap.org/data/2.5/weather?units=imperial&lat=';
+  $.getJSON(ipinfo, function(d){
     loc = d.loc.split(",");
-    console.log(loc);
+    console.log(location);
     console.log(d);
 
-    $.getJSON('http://api.openweathermap.org/data/2.5/weather?units=imperial&lat='
-              + loc[0] + '&lon=' + loc[1] +'&APPID=' + api_key, function(apiData){
+    $.getJSON(openAPI + loc[0] + '&lon=' + loc[1] +'&APPID=' + api_key, function(apiData){
       wd = apiData;
-      render(apiData, cel);
+      render(apiData, celsius);
       $('#toggle').click(function(){
-        cel = !cel;
-        render(wd, cel);
+        celsius = !celsius;
+        render(wd, celsius);
       })
     })
   })
 })
+});
